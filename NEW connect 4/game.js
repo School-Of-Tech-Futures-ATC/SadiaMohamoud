b95249369141d8b244b2dd711a242d96879ea287
Sadia Mohamoud <sadia.mohamoud@accenture.com>
2022-08-10 16:17:29 +0100

    add wins conditions

--- NEW connect 4/game.js
+++ NEW connect 4/game.js
@@ -9,25 +9,33 @@ User story 1:
 
 User story 2:
 *As two players of Connect 4, if I click reset, the board is cleared, such that the game can start again*
-- [ ] Winner message should be cleared
+- [✓] Winner message should be cleared
 - [✓] Red goes first after reset
 - [✓] After reset, games should continue to meet acceptance criteria of user story 1
 
 **User story 3:**
 
 *As two players of Connect 4, if either is able to place four discs of the same colour in a line, they are declared the winner*
-- [ ] A winner can be declared if a line occurs along any part of a row
-- [ ] A winner can be declared if a line occurs along any part of a column
-- [ ] A winner can be declared if a line occurs diagonally
-- [ ] If all positions are occupied and no lines are achieved, then nobody is declared the winner
-- [ ] If a winner is declared, then no further discs can be placed until the board is reset as per user story 2
+- [✓] A winner can be declared if a line occurs along any part of a row
+- [✓] A winner can be declared if a line occurs along any part of a column
+- [✓] A winner can be declared if a line occurs diagonally
+- [✓] If all positions are occupied and no lines are achieved, then nobody is declared the winner
+- [✓] If a winner is declared, then no further discs can be placed until the board is reset as per user story 2
+
+**User story 4:**
+
+*As a player of Connect 4, I can track my high score for the current game session (for this story a session won't persist upon a browser refresh)*
+- [ ] High scores shown after every game, no matter who wins
+- [ ] Score is calculated by subtracting the number of turns made before a winner is declared from 42
+- [ ] Scoreboard tracking player victories
+- [ ] High scores persist across multiple sessions of the game
 */
 
 let player1 = "red"
 let player2 = "yellow"
 let whosdraw= player1;
 let gameOver = false;
-let turnCounter = 0;
+let turn = 0;
 
 let board = [
     [null, null, null, null, null, null, null], 
@@ -62,84 +70,65 @@ function takeTurn(rows, columns) {
         }
 
 
-// Return either "red", "yellow" or "nobody" if the game is over.
-// Otherwise return null to continue playing.
-function checkWinner(p) {
+function checkWinner() {
     console.log("checkWinner was called");
-    for (int i=0;i<maxCol;i++)
-{
-    if (gridTable[rowNum][i]==player)
-        count++;
-    else
-        count=0;
 
-    if (count>=4)
-        return 1;
-}
-//Vertical check
-for (int i=0;i<maxRow;i++)
-{
-    if (gridTable[i][colNum]==player)
-        count++;
-    else
-        count=0;
-
-    if (count>=4)
-        return 1;
-} 
-count=0;
-// 4 in a row diagonally
-for(int i=colNum+1,j=rowNum+1;i<maxRow && j<maxCol;i++,j++) 
-{ 
-    if(gridTable[j][i]!=player)
-    {
-        count=1;
-        break;        
+    //check down
+    for (i = 0; i < 6; i++) {
+        for (j = 0; j < 4; j++){
+            if (board[i][j] &&
+                board[i][j] === board[i][j+1] &&
+                board[i][j+1] === board[i][j+2] &&
+                board[i][j+2] === board[i][j+3]){
+                    gameOver = true;
+                    console.log("Game has been won by", board[i][j])
+                    return board[i][j]
+                }
+        }
     }
-    count++;
-}
-// 4 in a row diagonally
-for(int i=colNum-1,j=rowNum-1;i>=0 && j>=0;i--,j--) 
-{ 
-    if(gridTable[j][i]!=player)
-    {
-        count=1;
-        break;        
+
+    // check right 
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 7; j++){
+            if (board[i][j] &&
+                board[i][j] === board[i+1][j] &&
+                board[i+1][j] === board[i+2][j] &&
+                board[i+2][j] === board[i+3][j]){
+                    gameOver = true;
+                    console.log("Game has been won by", board[i][j])
+                    return board[i][j]
+                }
+        }
     }
-    count++;
-}
-// 4 in a row diagonally
-for(int i=colNum+1,j=rowNum-1;i<maxRow && j>=0;i++,j--) 
-{ 
-    if(gridTable[j][i]!=player)
-    {
-        count=1;
-        break;        
+    // Check down-right
+    for (i = 0; i < 3; i++){
+        for (j = 0; j < 4; j++){
+            if (board[i][j] &&
+                board[i][j] === board[i+1][j+1] &&
+                board[i+1][j+1] === board[i+2][j+2] &&
+                board[i+2][j+2] === board[i+3][j+3]){
+                    gameOver = true;
+                    console.log("Game has been won by", board[i][j])
+                    return board[i][j]
+                }
+        }
     }
-    count++;
+   
+    // Check down-left
+    for (i = 3; i < 6; i++){
+        for (j = 0; j < 4; j++){
+            if (board[i][j] &&
+                board[i][j] === board[i-1][j+1] &&
+                board[i-1][j+1] === board[i-2][j+2] &&
+                board[i-2][j+2] === board[i-3][j+3]){
+                    gameOver = true;
+                    console.log("Game has been won by", board[i][j])
+                    return board[i][j]
+                }
+            }
+    } return null;
 }
 
-for(int i=colNum-1,j=rowNum+1;i>=0 && j<maxCol;i--,j++) 
-{ // 4 in a row diagonally
-    if(gridTable[j][i]!=player)
-    {
-        count=1;
-        break;        
-    }
-    count++;
-}
-
-if(count>=4)
-    return 1;
-
-return 0;
-}
-  
-    
-    return null;
-}
-
-// Set the game state back to its original state to play another game.
 function resetGame() {
     board = [
         [null, null, null, null, null, null, null], 
@@ -150,7 +139,7 @@ function resetGame() {
         [null, null, null, null, null, null, null]
     ]
     whosdraw = player1;
-    turnCounter = 0;
+    turn = 0;
     gameOver = false;
     console.log("resetGame was called");
     }
@@ -164,6 +153,7 @@ function getBoard() {
 
 if (typeof exports === 'object') {
     console.log("Running in Node")
+    
     // Node. Does not work with strict CommonJS, but only CommonJS-like 
     // environments that support module.exports, like Node.
     module.exports = {
@@ -175,6 +165,8 @@ if (typeof exports === 'object') {
 } else {
     console.log("Running in Browser")
 }
+
+
 
 
 
